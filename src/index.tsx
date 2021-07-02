300c69719f71d7c433d7f09b6691f3d24a5fa7de
Matt Gleich <git@mattglei.ch>
2021-07-02 02:09:00 -0400

    feat: clean up implementation
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,11 @@
 import { CopyToClipboardAction, List, ActionPanel, render } from '@raycast/api'
 import { emojis } from './emojiData'
 
-main()
-
-function main() {
-  render(
-    <List searchBarPlaceholder="Filter emojis by name...">
-      {...emojiList()}
-    </List>
-  )
-}
-
-// Generate a list of list view items for each emoji
-function emojiList(): Element[] {
-  const items: Element[] = []
-  for (let i = 0; i < emojis.length; i++) {
-    const emoji = emojis[i]
-    items.push(
+render(
+  <List searchBarPlaceholder="Filter emojis by name...">
+    {emojis.map((emoji, index) => (
       <List.Item
-        id={String(i)}
+        id={String(index)}
         title={emoji.name}
         subtitle={emoji.group}
         icon={emoji.char}
@@ -28,7 +15,6 @@ function emojiList(): Element[] {
           <CopyToClipboardAction title={'Copy Emoji'} content={emoji.char} />
         </ActionPanel>
       </List.Item>
-    )
-  }
-  return items
-}
+    ))}
+  </List>
+)
